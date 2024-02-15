f0ad1e3d8e6f0815ee75a645c59723c83b3eafc4
Yaroslav <makhinko180601@gmail.com>
2024-02-15 14:53:48 +0100

    fixed the display of transactions for a given day

--- src/components/history/HistoryIndex.vue
+++ src/components/history/HistoryIndex.vue
@@ -4,38 +4,34 @@
     <month-toggler/>
     <sumnary-balance/>
 
-    <div :key="currentDate" class="cont">
-      <div class="day" >
-        <p>{{ currentDate }}</p>
-        <p>-1125</p>
+    <div v-for="group in groupedTransactions" :key="group.date" class="cont">
+      <div class="day">
+        <p>{{ group.date }}</p>
+        <p>-1250</p>
       </div>
-        <div>
 
-          <div
-              v-for="transaction in transactionsStore.transactions"
-              :key="transaction.id"
-              class="shopping"
-              @click="editTransaction(transaction)"
-          >
-            <div class="icon-and-text">
-              <font-awesome-icon class="me-2  case"
-                                 :icon="categoriesStore.getCategory(transaction.category_id)?.icon "/>
-              <div>
-                <div class="subtext"> {{ transaction.description }}</div>
-                <div class="title">{{ categoriesStore.getCategory(transaction.category_id)?.title }}</div>
-              </div>
+      <div>
+        <div
+            v-for="transaction in group.transactions"
+            :key="transaction.id"
+            class="shopping"
+            @click="editTransaction(transaction)"
+        >
+          <div class="icon-and-text">
+            <font-awesome-icon class="me-2  case"
+                               :icon="categoriesStore.getCategory(transaction.category_id)?.icon "/>
+            <div>
+              <div class="subtext"> {{ transaction.description }}</div>
+              <div class="title">{{ categoriesStore.getCategory(transaction.category_id)?.title }}</div>
             </div>
-            <p>{{ transaction.amount }}</p>
           </div>
-
-
+          <p>{{ transaction.amount }}</p>
         </div>
-
       </div>
-
     </div>
+  </div>
 
-    <transaction-form/>
+  <transaction-form/>
 </template>
 
 
@@ -68,14 +64,40 @@ export default {
     editTransaction(transaction) {
       this.transactionsStore.formTransaction = {...transaction};
       this.transactionsStore.showForm = true;
-    }
+    },
+    getDateWithoutTime(dateString) {
+      return new Date(Date.parse(dateString)).toISOString().split('T')[0];
+    },
+
 
   },
   computed: {
     ...mapStores(useTransactionsStore, useCategoriesStore),
+    groupedTransactions() {
+
+
+      const groupedTransactions = [];
+
+      this.transactionsStore.transactions.forEach((transaction) => {
+        const date = this.getDateWithoutTime(transaction.spent_at);
+
+        let groupIndex = groupedTransactions.findIndex((item) => item.date === date);
+        if (groupIndex === -1) {
+          groupedTransactions.push({
+            date: date,
+            transactions: [],
+          });
+          groupIndex = groupedTransactions.length - 1;
+        }
+
+        groupedTransactions[groupIndex].transactions.push(transaction);
+      });
+
+      return groupedTransactions;
+    },
     currentDate() {
       const today = new Date();
-      const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+      const options = {year: 'numeric', month: 'numeric', day: 'numeric'};
       return today.toLocaleDateString(undefined, options);
     }
 
